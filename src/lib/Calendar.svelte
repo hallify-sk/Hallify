96a0b401e577397bd564b43cc78bcc7f743b9e98
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2024-02-09 07:11:03 +0100

    Start working on Calendar component

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
-    const currentTime = Date.now();
-    for(let week = 0; week < 7; week++){
-        for(let i = 0; i < 7; i++){
-            console.log(new Date(currentTime + 86400000 * (i+1) * (week+1)).toDateString());
-        };
-    };
+// You can get Calendar In ESM sytle
+import { Calendar } from 'headless-calendar';
+
+// Let's create a calendar of Ferbuary of 2024
+const february = Calendar.ofMonth(2024, 2, {startWeekdayIndex: 1});
+
+// `february` is an iterable. You can easily get useful info for each day like below:
+for (const day of february) {
+  console.log(`-------- ${day.day} ${day.monthName()} ${day.year} --------`);
+  console.log('Number of day:', day.count);
+  console.log('Day number of current month:', day.day);
+  console.log('Day number of current month in format:', day.dayInFormat());
+  console.log('Current Month number:', day.month);
+  console.log('Current year:', day.year);
+  console.log('Where the day is in a week:', day.weekdayIndex);
+  console.log('Where the day is in terms of passed weeks:', day.weekIndex);
+  console.log('Day name:', day.dayName());
+  console.log('Current month name:', day.monthName());
+  console.log(
+    'Is it the first start weekday of current month?',
+    day.isFirstStartWeekdayOfMonth
+  );
+  console.log('JavaScript Date object representing the day:', day.JSDate);
+
+	console.log(Calendar.ofYear(2024))
+
+}
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import Calendar from "$lib/Calendar.svelte";
-import Navbar from "$lib/Navbar.svelte";
+	import Calendar from '$lib/Calendar.svelte';
+	import Navbar from '$lib/Navbar.svelte';
 	export let data;
 	console.log(data);
 </script>
 
-<Navbar user={data.user}/>
+<Navbar user={data.user} />
 
-<Calendar/>
+<Calendar />
 
 <!--
 {#each data.stages as stage, i}
