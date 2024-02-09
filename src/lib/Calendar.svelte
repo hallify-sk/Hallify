4303cdce4bc34e1149a8aff5948a77e291cd82be
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2024-02-09 21:23:00 +0100

    Create basic calendar functionality

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -1,29 +1,91 @@
 <script lang="ts">
-// You can get Calendar In ESM sytle
-import { Calendar } from 'headless-calendar';
+	import { Calendar } from 'headless-calendar';
+	import { getMonthName, getNumberOfDaysInMonth, isToday } from './lib';
+	import { writable } from 'svelte/store';
+	import { onDestroy } from 'svelte';
 
-// Let's create a calendar of Ferbuary of 2024
-const february = Calendar.ofMonth(2024, 2, {startWeekdayIndex: 1});
+	const month = writable(new Date().getMonth() + 1);
+	const year = writable(new Date().getFullYear());
+	let monthData: Calendar;
 
-// `february` is an iterable. You can easily get useful info for each day like below:
-for (const day of february) {
-  console.log(`-------- ${day.day} ${day.monthName()} ${day.year} --------`);
-  console.log('Number of day:', day.count);
-  console.log('Day number of current month:', day.day);
-  console.log('Day number of current month in format:', day.dayInFormat());
-  console.log('Current Month number:', day.month);
-  console.log('Current year:', day.year);
-  console.log('Where the day is in a week:', day.weekdayIndex);
-  console.log('Where the day is in terms of passed weeks:', day.weekIndex);
-  console.log('Day name:', day.dayName());
-  console.log('Current month name:', day.monthName());
-  console.log(
-    'Is it the first start weekday of current month?',
-    day.isFirstStartWeekdayOfMonth
-  );
-  console.log('JavaScript Date object representing the day:', day.JSDate);
+	const unsubscribe = month.subscribe(() => {
+		monthData = Calendar.ofMonth($year, $month, { startWeekdayIndex: 1 });
+		console.log(monthData);
+	});
+	onDestroy(() => {
+		unsubscribe?.();
+	});
 
-	console.log(Calendar.ofYear(2024))
+    function addMonthHandleYear(){
+        if($month == 12){
+            console.log("hi");
+            year.set($year + 1);
+            month.set(1);
+        }else{
+            month.set($month + 1);
+        }
+    }
 
-}
+    function subtractMonthHandleYear(){
+        if($month == 1){
+            year.set($year - 1);
+            month.set(12);
+        }else{
+            month.set($month - 1);
+        }
+    }
+
+    //Prevents purging of css classes
+    ['col-start-1', 'col-start-2', 'col-start-3', 'col-start-4', 'col-start-5', 'col-start-6', 'col-start-7'];
+
 </script>
+
+<div class="flex flex-col mt-20">
+	<div class="flex flex-col">
+        <p>{getMonthName([...monthData][0].JSDate)} {$year}</p>
+		<div class="flex flex-row justify-evenly">
+			<button
+				on:click={subtractMonthHandleYear}
+				class="bg-blue-200 hover:bg-blue-300 w-full"
+			>
+				-
+			</button>
+			<button
+				on:click={addMonthHandleYear}
+				class="bg-blue-200 hover:bg-blue-300 w-full"
+			>
+				+
+			</button>
+		</div>
+		<div class="grid grid-cols-7 bg-blue-50 grid-rows-6 gap-1">
+            {#each Array([...monthData][0].weekdayIndex) as _, i}
+                <p class="
+                row-start-1
+                col-start-{[...monthData][0].weekdayIndex - i}
+                bg-blue-100 opacity-50 text-center grid place-items-center text-lg
+                ">{getNumberOfDaysInMonth($year, $month - 1) - i}</p>
+            {/each}
+			{#each [...monthData] as day}
+				<p
+					class="
+                {isToday(day.JSDate) ? 'text-red-500' : ''}
+                col-start-{day.weekdayIndex + 1}
+                bg-blue-100 hover:bg-blue-200 text-center grid place-items-center text-lg"
+				>
+					{day.day}
+				</p>
+			{/each}
+            {#each Array(42 - Array([...monthData][0].weekdayIndex).length - [...monthData].length) as _, i}
+             <!--
+                +2 because 
+                +1 for the grid-cols (it counts from 1 not 0)
+                +1 because i starts from 0 and it needs to start from 1
+             -->
+            <p class="
+            col-start-{([...monthData].at(-1)?.weekdayIndex || 0) + i + 2}
+            bg-blue-100 opacity-50 text-center grid place-items-center text-lg
+            ">{i + 1}</p>
+            {/each}
+		</div>
+	</div>
+</div>
--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,3 +1,23 @@
 export const serializeNonPOJOs = (obj: unknown) => {
     return JSON.parse(JSON.stringify(obj));
 };
+
+export function isToday(date: Date) {
+    const today = new Date();
+  
+    // 👇️ Today's date
+    console.log(today);
+  
+    if (today.toDateString() === date.toDateString()) {
+      return true;
+    }
+  
+    return false;
+}
+export function getMonthName(date: Date){
+    const month = date.toLocaleString('sk', { month: 'long' });
+    return month;
+}
+export function getNumberOfDaysInMonth(y: number, m: number){
+  return new Date(y, m, 0).getDate();
+}
