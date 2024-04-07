aaf1a6560e0e1546fed49d9975e38ef20da34de9
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2024-04-07 21:26:57 +0200

    Add a ruler
    Ruler has no functionality at this moment

--- src/lib/Brushes.svelte
+++ src/lib/Brushes.svelte
@@ -23,6 +23,9 @@
 				case 'l':
 					setBrush('zoneGrab');
 					break;
+				case 'r':
+					setBrush('ruler');
+					break;
 			}
 		});
 	});
@@ -175,5 +178,33 @@
 			></path>
 		</svg>
 		<p class="absolute left-12 w-28 overflow-hidden text-left">Wall grab</p>
+	</button>
+	<button
+		on:click={() => {
+			setBrush('ruler');
+		}}
+		class="w-full overflow-hidden h-12 rounded-md hover:bg-background-200 flex items-center relative justify-around gap-6 {$brush.type ==
+		'ruler'
+			? 'text-primary-600 bg-background-200'
+			: 'text-primary-400'}"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			class="w-6 h-6 absolute left-2 "
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+				d="M5 4h14a1 1 0 0 1 1 1v5a1 1 0 0 1 -1 1h-7a1 1 0 0 0 -1 1v7a1 1 0 0 1 -1 1h-5a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1"
+			/><path d="M4 8l2 0" /><path d="M4 12l3 0" /><path d="M4 16l2 0" /><path d="M8 4l0 2" /><path
+				d="M12 4l0 3"
+			/><path d="M16 4l0 2" /></svg
+		>
+		<p class="absolute left-12 w-28 overflow-hidden text-left">Ruler</p>
 	</button>
 </div>
