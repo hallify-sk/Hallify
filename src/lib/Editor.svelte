d9cbefa80dfc80b37755f8f76e69b4e6bc0585a7
Richard Marcinčák <em1t.dev@proton.me>
2024-06-23 00:06:02 +0200

    Add Circle class

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-	import { stage, Point2D, Vector2D } from "$lib/editor";
-	import { Stage, Layer, Rect } from "svelte-konva";
+	import { stage, Point2D, Vector2D, Circle2D } from "$lib/editor";
+	import { Stage, Layer, Rect, Circle } from "svelte-konva";
 
-    let pos = new Point2D(0, 0);
-    let pos2 = new Point2D(4, 5);
-    const vector = new Vector2D(pos, pos2);
-    console.log(vector.length);
+	let circle = new Circle2D(0, 0, 100); // Create a circle with center at (0,0) and radius 1
 
+	let points = circle.pointsOnCircumference(4, -10);
 </script>
 
 <Stage config={{ width: $stage.width, height: $stage.height, draggable: true }}>
-    <!--UI layer-->
+	<!--UI layer-->
 	<Layer>
-        <Rect
-            config={{x: 0, y: 0, width: $stage.width, height: $stage.height, fill: "red", draggable: false}}
-            />
-    </Layer>
-    <!--Non-collision objects layer-->
-    <!--Collision objects layer-->
+		<Rect config={{ x: 0, y: 0, width: $stage.width, height: $stage.height, fill: "red", draggable: false }} />
+	</Layer>
+	<!--Non-collision objects layer-->
+	<Layer>
+		{#each points as point}
+			<Rect config={{ x: point.x, y: point.y, offsetX: 2.5, offsetY: 2.5, width: 5, height: 5, fill: "blue", draggable: true, rotation: point.rotation }} />
+		{/each}
+        <Circle config={{ x: circle.x, y: circle.y, radius: circle.radius, fill: "transparent", stroke: "black", strokeWidth: 1 }} />
+	</Layer>
+	<!--Collision objects layer-->
 </Stage>
--- src/lib/editor.ts
+++ src/lib/editor.ts
@@ -12,9 +12,49 @@ export const stage = writable({
 export class Point2D {
     x: number;
     y: number;
-    constructor(x: number, y: number) {
+    rotation?: number;
+    constructor(x: number, y: number, rotation: number = 0) {
         this.x = x;
         this.y = y;
+        this.rotation = rotation;
+    }
+    distance(target: Point2D) {
+        return Math.sqrt(Math.pow(target.x - this.x, 2) + Math.pow(target.y - this.y, 2));
+    }
+};
+
+export class Circle2D {
+    x: number;
+    y: number;
+    radius: number;
+    constructor(x: number, y: number, radius: number) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    get area(){
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+    get circumference(){
+        return 2 * Math.PI * this.radius;
+    }
+    *pointsOnCircumference(points: number = 1, radiusOffset: number = 0){
+        if (points < 1) throw new Error("Points must be greater than 0");
+        for (let i = 0; i < points; i++){
+            yield new Point2D(
+                (this.radius + radiusOffset) *
+					1 * //Grid size
+					Math.cos((2 * Math.PI * i) / points),
+					//(grid.squareSize / 2) * grid.squaresPerMeter,
+				(this.radius + radiusOffset) *
+				    1 *
+					Math.sin((2 * Math.PI * i) / points),
+					//(grid.squareSize / 2) * grid.squaresPerMeter,
+                //this.x + (this.radius + radiusOffset) * Math.cos((i+degreeOffset) * Math.PI / 180),
+                //this.y + (this.radius + radiusOffset) * Math.sin((i+degreeOffset) * Math.PI / 180),
+                (i * 360) / points
+            );
+        }
     }
 };
 
@@ -26,12 +66,29 @@ export class Vector2D {
         this.destination = destination;
     }
     get length() {
-        return Math.sqrt(Math.pow(this.destination.x - this.origin.x, 2) + Math.pow(this.destination.y - this.origin.y, 2));
+        return Math.sqrt(
+            Math.pow(
+                this.destination.x - this.origin.x, 2
+            ) + Math.pow(
+                this.destination.y - this.origin.y, 2
+            )
+        );
     }
 };
 
 export class CollisionManager {
-    static checkCollision(a: Vector2D, b: Vector2D) {
-        return a.origin.x < b.destination.x && a.destination.x > b.origin.x && a.origin.y < b.destination.y && a.destination.y > b.origin.y;
+    //Returns objects that are in proximity to the target object
+    static findProximity(target: Point2D, objects: Array<Point2D>, radius: number){
+        return objects.filter(object => target.distance(object) <= radius);
     }
-}
+    static pointPoint(pointOne: Point2D, pointTwo: Point2D){
+        return pointOne.distance(pointTwo) === 0;
+    }
+    static pointCircle(point: Point2D, circle: Circle2D){
+        return point.distance(new Point2D(circle.x, circle.y)) <= circle.radius;
+    }
+    static circleCircle(circleOne: Circle2D, circleTwo: Circle2D){
+        return new Point2D(circleOne.x, circleOne.y)
+            .distance(new Point2D(circleTwo.x, circleTwo.y)) <= circleOne.radius + circleTwo.radius;
+    }
+};
